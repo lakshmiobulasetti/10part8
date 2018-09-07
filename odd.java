b760304a1f506df905a41615b6451e089679de11
lakshmiobulasetti <41731019+lakshmiobulasetti@users.noreply.github.com>
2018-09-07 11:13:46 +0530

    Update and rename README.md to odd.java

---
+++ odd.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	String s;
+System.out.println("Input anything you want: ");
+Scanner scanner = new Scanner(System.in);
+s = scanner.nextLine();
+
+int n = 10;
+System.out.println("Odd Numbers are");
+for(int i=1;i<=n;i++) {
+  if(i%2!=0) {
+    System.out.print(i + " ");
+  }
+}
+}
+}
+	
